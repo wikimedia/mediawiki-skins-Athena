112c3506e4705bed0e5b4b48d5a86da8b6bc1907
jdlrobson <jdlrobson@gmail.com>
2021-01-04 08:43:57 -0800

    Restore styles to Athena
    Bug: T271068
    Change-Id: I0bdf2e70b2b445791dc9cc35960bde8dc5c83ec5

--- includes/SkinAthena.php
+++ includes/SkinAthena.php
@@ -1,19 +1,6 @@
 <?php
 
 class SkinAthena extends SkinTemplate {
-
-	public $skinname = 'athena';
-
+	/** @var string Needed for 1.35 support. This property and entire class can be removed in 1.36. */
 	public $template = 'AthenaTemplate';
-
-	/**
-	 * @param OutputPage &$out
-	 * @param Skin &$skin
-	 */
-	static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
-		global $wgDefaultSkin;
-		if ( $wgDefaultSkin === 'athena' || $wgDefaultSkin === 'Athena' ) {
-			$out->addModuleStyles( 'skins.athena' );
-		}
-	}
 }
